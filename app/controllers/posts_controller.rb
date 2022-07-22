107e27cf56ad803e50d8fbb1385a0da04d4f0ad4
ericjbarton <erockbart@gmail.com>
2022-07-22 09:38:39 -0400

    adds a bunch of stuff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,10 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.all
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json { render :json => @posts }
+    end
   end
 
   # GET /posts/1 or /posts/1.json
@@ -58,13 +62,14 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:name, :title, :content)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:name, :title, :content)
+  end
 end
